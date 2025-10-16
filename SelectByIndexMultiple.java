4fa37f34d0648a68789faa9c08b03f04e5ef842b
Ketan Narhe <ketannarhepatil@gmail.com>
2025-10-16 19:37:38 +0530

    Add files via upload

---
+++ SelectByIndexMultiple.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SelectByIndexMultiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new FirefoxDriver(); 
+		driver.get("http://ironspider.ca/forms/dropdowns.htm"); 
+		WebElement w = driver.findElement(By.name("coffee2")); 
+		
+		Select s=new Select(w);
+		
+		List<WebElement> web=s.getOptions();
+	   {
+			for(int i=0;i<web.size();i++)
+			{
+				s.selectByIndex(i);
+			}
+	   }
+		
+
+	}
+
+}
