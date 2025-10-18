23336c599ca1c3d5f447a8bb31dff705d799994a
Ketan Narhe <ketannarhepatil@gmail.com>
2025-10-18 17:41:17 +0530

    Add files via upload

---
+++ WithoutDragAndDrop.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class WithoutDragAndDrop {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver(); 
+		driver.manage().window().maximize(); 
+		driver.get("http://demo.guru99.com/test/drag_drop.html"); 
+		
+		Actions a = new Actions(driver); 
+		
+		WebElement source = driver.findElement(By.xpath("//a[text()=' BANK']")); 
+		WebElement des =driver.findElement(By.xpath("(//li[@class='placeholder'])[1]"));
+		
+		Thread.sleep(1000);
+		
+		a.clickAndHold(source).moveToElement(des).release(des).perform();
+		
+	}
+
+}
